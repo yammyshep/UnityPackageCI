54d97b5610643448634dc9eb5def3a24ec58bf00
YammyShep <jdbuelow@gmail.com>
2022-12-06 21:51:47 -0600

    this test should fail

--- Tests/TestTests.cs
+++ Tests/TestTests.cs
@@ -12,7 +12,7 @@ namespace TestPackage.Tests
         [Test]
         public void TestTestsSimplePasses()
         {
-            // Use the Assert class to test conditions
+            Assert.Fail();
         }
 
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
