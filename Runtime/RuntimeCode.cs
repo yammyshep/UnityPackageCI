c7084cabcd50644591e92861b933e5b110f234cb
YammyShep <jdbuelow@gmail.com>
2022-12-08 14:14:09 -0600

    Documentation building

---
+++ Runtime/RuntimeCode.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace TestPackage.Runtime
+{
+    public class SomeRuntimeCode : MonoBehaviour
+    {
+        /// <summary>
+        /// This is some summary text for a function :)
+        /// </summary>
+        public void SomeFunction(int paramA, bool paramB)
+        {
+            return;
+        }
+    }
+}
